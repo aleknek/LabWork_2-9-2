6a10f848ba59e36925e4c207a5e411c841f761fd
Your Name <you@example.com>
2016-07-09 23:58:45 +0300

    Initial commit

---
+++ src/com/brainacad/oop/teststring2/Main.java
@@ -0,0 +1,16 @@
+package com.brainacad.oop.teststring2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myStr1 = "Cartoon";
+        String myStr2 = "Tomcat";
+
+        for (int i = 0; i < myStr1.length(); i++) {
+            if (myStr2.indexOf(myStr1.charAt(i)) == -1) {
+                System.out.println(myStr1.charAt(i));
+            }
+        }
+    }
+}
